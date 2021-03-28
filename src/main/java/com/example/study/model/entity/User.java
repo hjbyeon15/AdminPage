6ae22e0fe12f5e0b58b995c1f5c0127ff1538207
hjbyeon15@gmail.com <comdat88@gmail.com>
2021-03-28 23:52:02 +0900

    CRUD with JPA

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -2,11 +2,13 @@ package com.example.study.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 //@Table(name="user") // 클래스의 이름과 테이블 이름이 동일하다면 선언하지 않아도 됨.
