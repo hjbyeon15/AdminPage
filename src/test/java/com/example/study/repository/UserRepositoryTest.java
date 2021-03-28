70b97a4c96cfb05422f2437a4a9ffbb898039d03
hjbyeon15@gmail.com <comdat88@gmail.com>
2021-03-29 00:17:57 +0900

    CRUD with JPA_2

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
     //Dependency Injection(DI)
@@ -15,8 +16,8 @@ public class UserRepositoryTest extends StudyApplicationTests {
     @Test
     public void create() {
         User user = new User();
-        user.setAccount("TestUser01");
-        user.setEmail("TestUser01@gmail.com");
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
         user.setPhoneNumber("010-1111-1111");
         user.setCreatedAt(LocalDateTime.now());
         user.setCreatedBy("admin");
@@ -24,10 +25,28 @@ public class UserRepositoryTest extends StudyApplicationTests {
         User newUser = userRepository.save(user);
         System.out.println("newUser : "+newUser);
     }
-    public void read() {
 
+    @Test
+    public void read() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{
+            System.out.println("user: "+selectUser);
+            System.out.println("email: "+selectUser.getEmail());
+        });
     }
+    @Test
     public void update() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
+
 
     }
     public void delete() {
