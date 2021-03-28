f3d9cd19fa72764a209c0a1f4a61ae9185afd816
hjbyeon15@gmail.com <comdat88@gmail.com>
2021-03-28 23:36:12 +0900

    Entity and Repository

---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Entity
+//@Table(name="user") // 클래스의 이름과 테이블 이름이 동일하다면 선언하지 않아도 됨.
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String account;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+
+
+}
